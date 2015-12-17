4952da33d98322fddd015d9250abaabf79051cef
natalie <natalie@tridgell.net>
2015-12-18 02:24:43 +1100

    Alter gen logs script to consider size
    Now takes multiple sizes and tests with range = 2 * size

--- c-cpp/scripts/gen_logs.sh
+++ c-cpp/scripts/gen_logs.sh
@@ -1,13 +1,11 @@
-#! /bin/bash
-
 DIR=".."
 OUTPUT=${DIR}/output
 BIN=${DIR}/bin
 
 SYNC="LOCKFREE"
-THREADS="1 2 4"
-SIZE=256
-UPDATES="10 25"
+THREADS="1 2 4 8 16 24 32"
+SIZE="4096 65536"
+UPDATES="0 20 100"
 ITER=10
 
 
@@ -23,15 +21,18 @@ mkdir ${OUTPUT}/log ${OUTPUT}/data ${OUTPUT}/analysis
 # records all benchmark outputs
 ###############################
 
-# lockfree benchmarks
-BENCHES="versioned-skiplist"
-for bench in ${BENCHES}; do
-  for t in ${THREADS}; do
-    for u in ${UPDATES}; do
-      OUT=${OUTPUT}/log/${bench}-u${u}-t${t}.log
-      echo "Starting to write to ${OUT}"
-      for (( j=1; j<=${ITER}; j++ )); do
-        ${BIN}/${bench} -u ${u} -t ${t} -i ${SIZE} >> ${OUT}
+# skiplist benchmarks
+BENCHES="versioned-skiplist lockfree-fraser-skiplist MUTEX-skiplist"
+for i in ${SIZE}; do
+  r=`expr $i \* 2`
+  for bench in ${BENCHES}; do
+    for t in ${THREADS}; do
+      for u in ${UPDATES}; do
+        OUT=${OUTPUT}/log/${bench}-u${u}-t${t}-i${i}.log
+        echo "Writing to ${OUT}"
+        for (( j=1; j<=${ITER}; j++ )); do
+          ${BIN}/${bench} -u ${u} -t ${t} -i ${i} -r ${r} >> ${OUT}
+        done
       done
     done
   done
